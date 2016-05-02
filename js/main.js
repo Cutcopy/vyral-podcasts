bf12296941336cff4b332983f691084788f8bd10
Bryce Stradling <bryce@getvyral.com>
2016-05-02 17:10:45 -0500

    adding audio player js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,24 @@ $(document).ready(function(){
     });
 
 });
+
+$(document).ready(function(){
+ $("#jquery_jplayer_1").jPlayer({
+   ready: function () {
+     $(this).jPlayer("setMedia", {
+       title: "Bubble",
+       m4a: "https://s3.amazonaws.com/vyralmarketing/vyral-marketing/12+Love.mp3",
+       oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
+     });
+   },
+   cssSelectorAncestor: "#jp_container_1",
+   swfPath: "/js",
+   supplied: "m4a, oga",
+   useStateClassSkin: true,
+   autoBlur: false,
+   smoothPlayBar: true,
+   keyEnabled: true,
+   remainingDuration: true,
+   toggleDuration: true
+ });
+});
