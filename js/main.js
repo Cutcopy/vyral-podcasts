f7e460faf75b7320e7fb804e83f30fb5d82100ce
Bryce Stradling <bryce@getvyral.com>
2016-05-03 16:56:49 -0500

    added about page, more audio customizations.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,8 @@
 $(document).ready(function(){
+  //caret flip 
     $('.industry-drop').click(function(){
       $('#industries').slideToggle("slow");
       $('.caret').toggleClass("rotate");
     });
 
-});
-
-$(document).ready(function(){
- $("#jquery_jplayer_1").jPlayer({
-   ready: function () {
-     $(this).jPlayer("setMedia", {
-       title: "Bubble",
-       m4a: "https://s3.amazonaws.com/vyralmarketing/vyral-marketing/12+Love.mp3",
-       oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg"
-     });
-   },
-   cssSelectorAncestor: "#jp_container_1",
-   swfPath: "/js",
-   supplied: "m4a, oga",
-   useStateClassSkin: true,
-   autoBlur: false,
-   smoothPlayBar: true,
-   keyEnabled: true,
-   remainingDuration: true,
-   toggleDuration: true
- });
 });
